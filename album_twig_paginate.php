43a7eb7cdfd5a0a2cd2397b3240785c97c07c7a0
namrehkok@gmail.com <namrehkok@gmail.com>
2017-03-10 09:39:40 +0100

    Started with pagination Changed position of buttons frnt and star in gui

---
+++ album_twig_paginate.php
@@ -0,0 +1,71 @@
+<?php
+include 'settings.php';
+include 'header.html';
+
+// Setting up Twig
+require_once __DIR__ . '/vendor/autoload.php';
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates//');
+$twig = new Twig_Environment($loader);
+
+// Connecting, selecting database
+$dbconn = pg_connect("host=localhost dbname=album user=album password=album")
+    or die('Could not connect: ' . pg_last_error());
+
+// get the sorting order of the pictures
+switch ( $_GET['sorting'])
+{
+  case 'desc':
+    $sorting = 'desc';
+    break;
+  default:
+    $sorting = 'asc';
+    break;
+}
+
+switch ( $_GET['offset'])
+{
+  case '':
+    $offset = 1;
+    break;
+  default:
+    $offset = $_GET['offset'];
+    break;
+}
+
+echo $offset;
+
+// Only getting the favorite pictures in case the favorite=1 was set in URL
+switch ( $_GET['favorite'] )
+  {
+    case '1':
+      $query = "SELECT a.name, b.* FROM albums a join media b on a.id = b.album_id and b.favorite = True and a.slug = '" . $_GET['album'] ."' order by b.createddate " . $sorting . " limit 10 offset " . $offset .";";
+      break;
+    default:
+      $query = "SELECT a.name, b.* FROM albums a join media b on a.id = b.album_id and a.slug = '" . $_GET['album'] ."' order by b.createddate " . $sorting . " limit 10 offset " . $offset .";";
+      break;
+  }
+
+echo $query;
+
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+$data = pg_fetch_all($result);
+
+// Sending data to template
+echo $twig->render('album_paginate.twig', array('data' => $data, 'thumbs' => $thumbs,
+        'originelen' => $originelen,
+        'url_media' => $url_media,
+        'url_toggle_favorite' => $url_toggle_favorite,
+        'url_toggle_front' => $url_toggle_front,
+        'url_album' => $url_album,
+        'album' => $_GET['album']
+      ));
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+
+include 'footer.html';
+?>
---
+++ updates.sql
@@ -0,0 +1,112 @@
+﻿select * from
+(
+	select 
+	rank() over (partition by album_id, x, xx order by original desc) rnk
+	, lag(createddate) over (partition by album_id order by original desc) prev_created
+	, t.* 
+	from
+	(
+		select 
+		case when createddate is null then 0 else 1 end x,
+		lag(case when createddate is null then 0 else 1 end) over (partition by album_id order by original desc) xx
+		, t.* 
+		from media t where album_id = 110
+		order by original desc
+	) t
+) u
+--where x+xx = 1
+order by original desc
+
+
+
+select
+id, max(expire) over (order by id rows between unbounded preceding and current row) as expire
+from t
+order by id desc
+
+update media set
+createddate = ad
+select * from
+(
+select 
+t.*,
+max(createddate) over (partition by album_id order by original rows between unbounded preceding and current row) ad
+from 
+media t
+) t
+where (createddate < current_date - 21000 or createddate is null)
+and id = 97
+
+
+update media 
+set createddate = (
+select ad from
+(
+select 
+t.*,
+max(createddate) over (partition by album_id order by original rows between unbounded preceding and current row) ad
+from 
+media t
+) t
+where (createddate < current_date - 21000 or createddate is null)
+and t.id = media.id and t.id = 306
+and exists (select 1 from media x where (createddate < current_date - 26000 or createddate is null) and media.id = x.id)
+)
+
+
+create table media_backup_t as
+select * from media_backup
+
+select * from media a join media_backup b on a.id = b.id where a.createddate <> coalesce(b.createddate, current_date)
+select * from media_backup where id = 97
+select * from media where id = 97
+
+select * from
+(
+select 
+t.*,
+max(createddate) over (partition by album_id order by original rows between unbounded preceding and current row) ad
+from 
+media t
+) t
+where (createddate < current_date - 21000 or createddate is null)
+and t.id = 306
+
+select * from media where createddate is not null and id = 97
+
+drop table test;
+create table test
+(id integer,
+a integer);
+
+insert into test values (1,1);
+insert into test values (1,2);
+insert into test values (1,null);
+
+select * from test
+
+update test set a = (select max(a) from test t where test.id = t.id group by id)
+
+
+update media as v
+set createddate = s.ad
+from
+(
+	select * from
+	(
+	select 
+	t.*,
+	max(createddate) over (partition by album_id order by original rows between unbounded preceding and current row) ad
+	from 
+	media t
+	) t
+	where t.createddate is null
+)
+as s
+where s.id = v.id
+
+
+
+select * from media limit 50 offset 90000
+
+select * from albums  where name like '%namreh%'
